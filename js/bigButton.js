a13fc89b0f941b487dddd24197e16871b7204c6f
cobbland <jacobdensford@proton.me>
2025-07-29 08:35:39 -0400

    Center main content when big

--- js/bigButton.js
+++ js/bigButton.js
@@ -1,10 +1,12 @@
 const header = document.getElementById("header");
 const footer = document.getElementById("footer");
+const main = document.querySelector("main");
 const button = document.getElementById("big-button");
 const contentWindow = document.querySelector(".content-window");
 
 button.addEventListener("click", () => {
     header.classList.toggle("invisible");
     footer.classList.toggle("invisible");
-    contentWindow.classList.toggle("big-window");
+    contentWindow.classList.toggle("big-window")
+    main.classList.toggle("main-big");
 });
