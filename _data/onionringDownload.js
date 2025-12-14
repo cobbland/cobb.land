d123445dd90fafe8b15c6d0c9ebf9950a2fb7eec
Jacob Densford <hello@jacobdensford.com>
2025-12-14 17:15:39 -0500

    Fix random blog button take three

--- _data/onionringDownload.js
+++ _data/onionringDownload.js
@@ -1,45 +1,18 @@
-// Change https to http if the files below are hosted at an insecure domain
 import https from 'https';
 import fs from 'fs';
 
-// Set ringVars to the address of your webring's onionring-variables.js file.
-const ringVars = 'https://woodpaneled.org/scripts/onionring-variables.js';
-// Set ringWidget to the address of your webring's onionring-widget.js file.
-const ringWidget = 'https://woodpaneled.org/scripts/onionring-widget.js';
-// Set ringVarsFile to the location and filename where you want to save the above.
-const ringVarsFile = fs.createWriteStream('data/onionring-variables.js');
-// Set ringVarsFile to the location and filename where you want to save the above.
-const ringWidgetFile = fs.createWriteStream('data/onionring-widget.js');
+const ringVarsUrl = 'https://woodpaneled.org/scripts/onionring-variables.js';
+const ringVarsFile = 'data/onionring-variables.js';
 
-let gotRingVars = false;
-let gotRingWidget = false;
-
-const ringVarsRequest = https.get(ringVars, ((res) => {
-    res.pipe(ringVarsFile);
-    if (res.statusCode === 200) {
-        ringVarsFile.on('finish', () => {
-            ringVarsFile.close();
-            gotRingVars = true;
-            console.log("Downloaded: onionring-variables.js");
-            fs.appendFileSync('data/onionring-variables.js', '\nexport {woodPaneledSites};');
-            console.log('Appended export statement!');
-        });
-    } else {
-        ringVarsFile.close();
-        console.error('Something is not right. Double check ringVars');
-    }
-}));
-
-const ringWidgetRequest = https.get(ringWidget, ((res) => {
-    res.pipe(ringWidgetFile);
-    if (res.statusCode === 200) {
-        ringWidgetFile.on('finish', () => {
-            ringWidgetFile.close();
-            gotRingWidget = true;
-            console.log("Downloaded: onionring-widget.js");
-        });
-    } else {
-        ringWidgetFile.close();
-        console.error('Something is not right. Double check ringWidget');
-    }
-}));
+https.get(ringVarsUrl, (res) => {
+    let data = '';
+    res.on('data', chunk => data += chunk);
+    res.on('end', () => {
+        if (res.statusCode === 200) {
+            fs.writeFileSync(ringVarsFile, data + '\nexport {woodPaneledSites};');
+            console.log('Downloaded, wrote, and appended onionring-variables.js');
+        } else {
+            console.error('Failed to download onionring-variables.js');
+        }
+    });
+});
