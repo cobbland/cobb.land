5afdee0852fcda060ed83a193c6b143a9ebc4e08
Jacob Densford <hello@jacobdensford.com>
2025-12-14 18:21:15 -0500

    Add code to generate next and last link for woodpaneled

--- _data/onionringDownload.js
+++ _data/onionringDownload.js
@@ -16,3 +16,4 @@ https.get(ringVarsUrl, (res) => {
         }
     });
 });
+
---
+++ _data/onionringLinks.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import * as cheerio from 'cheerio';
+import { woodPaneledSites } from '../data/onionring-variables.js';
+
+const ownUrl = 'https://jacobdensford.com/'
+
+const htmlPath = '_includes/layout.html';
+const html = fs.readFileSync(htmlPath, 'utf-8');
+const $ = cheerio.load(html);
+let lastLink = woodPaneledSites[
+    (woodPaneledSites.findIndex(item => item.url === ownUrl)) - 1
+].url;
+let nextLink = woodPaneledSites[
+    (woodPaneledSites.findIndex(item => item.url === ownUrl)) + 1
+].url;
+$("#woodpaneled-last").attr("href", lastLink);
+$("#woodpaneled-next").attr("href", nextLink);
+fs.writeFileSync(htmlPath, $.html(), 'utf-8');
